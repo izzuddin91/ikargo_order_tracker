b08071345cf3fd71802f506256684f4b4a65c490
izzuddin91 <your.izzuddin91@me.com>
2015-08-26 18:35:59 +0800

    csv attachment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
 	before_filter :authenticate_user!
 	
-
 	def index
-		@order = Order.new
+		@order = Order.new(delivered: "OTW")
+		@orders = Order.select(:delivered).distinct
 		@address = []
 		@address << Order.pluck(:pick_up_address_1)*","
 		@address << Order.pluck(:pick_up_address_2)*","
@@ -27,7 +27,6 @@ class OrdersController < ApplicationController
 		end
 	end
 
-
 	def dashboard
 		@order = Order.all
 		respond_to do |format|
@@ -39,77 +38,23 @@ end
 
 
 def create
+	
 	a = current_user.email
-	@order = Order.create(
-		user_email: a,
-		inquiry_date: params[:order][:inquiry_date], 
-		no: params[:order][:no], 
-		shipper: params[:order][:shipper],
-		shipper_address: params[:order][:shipper_address],
-		transporter: params[:order][:transporter], 
-		truck_size: params[:order][:truck_size],
-		payment_rate: params[:order][:payment_rate], 
-		payment_advance: params[:order][:payment_advance], 
-		payment_paid: params[:order][:payment_paid], 
-		payment_commision: params[:order][:payment_commision], 
-		payment_balance: params[:order][:payment_balance], 
-		remarks: params[:order][:remarks], 
-		pick_up_address_1: params[:order][:pick_up_address_1],
-		pick_up_pic_no_1: params[:order][:pick_up_pic_no_1],
-		pick_up_address_2: params[:order][:pick_up_address_2],
-		pick_up_pic_no_2: params[:order][:pick_up_pic_no_2],
-		pick_up_address_3: params[:order][:pick_up_address_3],
-		pick_up_pic_no_3: params[:order][:pick_up_pic_no_3],
-		pick_up_address_4: params[:order][:pick_up_address_4],
-		pick_up_pic_no_4: params[:order][:pick_up_pic_no_4],
-		drop_point_address_1: params[:order][:drop_point_address_1],
-		drop_point_pic_no_1: params[:order][:drop_point_pic_no_1],
-		drop_point_address_2: params[:order][:drop_point_address_2],
-		pick_up_pic_no_2: params[:order][:pick_up_pic_no_2],
-		drop_point_address_3: params[:order][:drop_point_address_3],
-		pick_up_pic_no_3: params[:order][:pick_up_pic_no_3],
-		drop_point_address_4: params[:order][:drop_point_address_4],
-		pick_up_pic_no_4: params[:order][:pick_up_pic_no_4],
-		goods_description: params[:order][:goods_description],
-		datetime: params[:order][:datetime],
-		special_request: params[:order][:special_request],
-		drop_point_date: params[:order][:drop_point_date],
-		trucks_capacity: params[:order][:trucks_capacity],
-		truck_reg_no: params[:order][:truck_reg_no],
-		name: params[:order][:name],
-		ic: params[:order][:ic],
-		phone_no: params[:order][:phone_no],
-		delivery_date: params[:order][:delivery_date],
-		do_request_date: params[:order][:do_request_date],
-		do_collection_date: params[:order][:do_collection_date],
-		do_number: params[:order][:do_number],
-		total_tonne: params[:order][:total_tonne],
-		payment_name: params[:order][:payment_name],
-		tel_no: params[:order][:tel_no],
-		account_no: params[:order][:account_no],
-		billing_name: params[:order][:billing_name],
-		billing_amount: params[:order][:billing_amount],
-		billing_received: params[:order][:billing_received],
-		billing_balance: params[:order][:billing_balance],
-		invoice_date: params[:order][:invoice_date],
-		invoice: params[:order][:invoice],
-		delivered: params[:delivered],
-		payment_calculate_gst: params[:order][:payment_calculate_gst],
-		payment_reverse_calculate_gst: params[:order][:payment_reverse_calculate_gst],
-		gst_total_value: params[:order][:gst_total_value],
-		payment_with_gst: params[:order][:payment_with_gst],
-		billing_with_gst: params[:order][:billing_with_gst],
-		billing_calculate_gst: params[:order][:billing_calculate_gst],
-		billing_reverse_calculate_gst: params[:order][:billing_reverse_calculate_gst],
-		gst_billing_total_value: params[:order][:gst_billing_total_value],
-		rate: params[:order][:rate],
-		notes: params[:order][:notes],
-		billing_rate: params[:order][:billing_rate],
-		driver_remarks: params[:order][:driver_remarks]
-		)
-# UserMailer.create_edit_order(@order).deliver
-@records = Record.create(order_id: @order.id, changer: @order.user_email.split("@")[0], rate_history: @order.payment_with_gst)
-redirect_to root_path(@order)
+	@order = Order.create(order_params)
+	@order.update(user_email: a)
+	@records = Record.create(order_id: @order.id, changer: @order.user_email.split("@")[0], rate_history: @order.payment_with_gst)
+  file = Tempfile.new(["a", ".csv"]) #do |csv|
+
+     # csv << ["Application"]
+     #  csv << "\n"
+     #  csv << [@order.inquiry_date]
+ 
+    	UserMailer.create_edit_order(@order, file).deliver
+
+    # end
+
+
+	redirect_to root_path(@order)
 end
 
 
@@ -133,78 +78,13 @@ end
 def update
 	@order = Order.find(params[:id])
 	a = current_user.email
-	@order.update(
-		user_email: a,
-		inquiry_date: params[:order][:inquiry_date], 
-		no: params[:order][:no], 
-		shipper: params[:order][:shipper],
-		shipper_address: params[:order][:shipper_address],
-		transporter: params[:order][:transporter], 
-		truck_size: params[:order][:truck_size],
-		payment_rate: params[:order][:payment_rate], 
-		payment_advance: params[:order][:payment_advance], 
-		payment_paid: params[:order][:payment_paid], 
-		payment_commision: params[:order][:payment_commision], 
-		payment_balance: params[:order][:payment_balance], 
-		remarks: params[:order][:remarks], 
-		pick_up_address_1: params[:order][:pick_up_address_1],
-		pick_up_pic_no_1: params[:order][:pick_up_pic_no_1],
-		pick_up_address_2: params[:order][:pick_up_address_2],
-		pick_up_pic_no_2: params[:order][:pick_up_pic_no_2],
-		pick_up_address_3: params[:order][:pick_up_address_3],
-		pick_up_pic_no_3: params[:order][:pick_up_pic_no_3],
-		pick_up_address_4: params[:order][:pick_up_address_4],
-		pick_up_pic_no_4: params[:order][:pick_up_pic_no_4],
-		drop_point_address_1: params[:order][:drop_point_address_1],
-		drop_point_pic_no_1: params[:order][:drop_point_pic_no_1],
-		drop_point_address_2: params[:order][:drop_point_address_2],
-		pick_up_pic_no_2: params[:order][:pick_up_pic_no_2],
-		drop_point_address_3: params[:order][:drop_point_address_3],
-		pick_up_pic_no_3: params[:order][:pick_up_pic_no_3],
-		drop_point_address_4: params[:order][:drop_point_address_4],
-		pick_up_pic_no_4: params[:order][:pick_up_pic_no_4],
-		goods_description: params[:order][:goods_description],
-		datetime: params[:order][:datetime],
-		special_request: params[:order][:special_request],
-		drop_point_date: params[:order][:drop_point_date],
-		trucks_capacity: params[:order][:trucks_capacity],
-		truck_reg_no: params[:order][:truck_reg_no],
-		name: params[:order][:name],
-		ic: params[:order][:ic],
-		phone_no: params[:order][:phone_no],
-		delivery_date: params[:order][:delivery_date],
-		do_request_date: params[:order][:do_request_date],
-		do_collection_date: params[:order][:do_collection_date],
-		do_number: params[:order][:do_number],
-		total_tonne: params[:order][:total_tonne],
-		payment_name: params[:order][:payment_name],
-		tel_no: params[:order][:tel_no],
-		account_no: params[:order][:account_no],
-		billing_name: params[:order][:billing_name],
-		billing_amount: params[:order][:billing_amount],
-		billing_received: params[:order][:billing_received],
-		billing_balance: params[:order][:billing_balance],
-		invoice_date: params[:order][:invoice_date],
-		invoice: params[:order][:invoice],
-		delivered: params[:delivered],
-		payment_calculate_gst: params[:order][:payment_calculate_gst],
-		payment_reverse_calculate_gst: params[:order][:payment_reverse_calculate_gst],
-		gst_total_value: params[:order][:gst_total_value],
-		payment_with_gst: params[:order][:payment_with_gst],
-		billing_with_gst: params[:order][:billing_with_gst],
-		billing_calculate_gst: params[:order][:billing_calculate_gst],
-		billing_reverse_calculate_gst: params[:order][:billing_reverse_calculate_gst],
-		gst_billing_total_value: params[:order][:gst_billing_total_value],
-		rate: params[:order][:rate],
-		notes: params[:order][:notes],
-		billing_rate: params[:order][:billing_rate],
-		driver_remarks: params[:order][:driver_remarks]
-		)
-if current_user.email != "mu@ikargo.com"
+	@order.update(order_params)
+	@order.update(user_email: a)
+	if current_user.email != "mu@ikargo.com"
 # UserMailer.edit_order(@order).deliver
 end
 if @order.payment_with_gst != Record.last.rate_history
-@records = Record.create(order_id: @order.id, changer: @order.user_email.split("@")[0], rate_history: @order.payment_with_gst)
+	@records = Record.create(order_id: @order.id, changer: @order.user_email.split("@")[0], rate_history: @order.payment_with_gst)
 end
 redirect_to root_path(@order)
       # Handle a successful update.
@@ -234,44 +114,31 @@ end
 private
 
 def order_params
-	params.require(:order).permit(
-		:inquiry_date , 
-		:no , 
-		:shipper ,
-		:transporter , 
-		:truck_size ,
-		:rate , 
-		:remarks , 
-		:address ,
-		:goods_description ,
-		:datetime ,
-		:pick_up_pic_no ,
-		:special_request ,
-		:drop_point_date ,
-		:drop_point_pic_no ,
-		:trucks_capacity ,
-		:truck_reg_no ,
-		:name ,
-		:ic ,
-		:phone_no ,
-		:delivery_date ,
-		:do_request_date ,
-		:do_collection_date ,
-		:do_number ,
-		:total_tonne ,
-		:payment_name ,
-		:tel_no ,
-		:account_no ,
-		:advance ,
-		:paid ,
-		:balance ,
-		:billing_name ,
-		:amount ,
-		:invoice_date ,
-		:invoice ,
-		:payment_collection 
+
+	params.require(:order).permit(:inquiry_date, :no, :shipper, :shipper_address, :transporter, :truck_size, :payment_rate,
+		:payment_advance, :payment_paid, :payment_commision, :payment_balance, :remarks, :pick_up_address_1, :pick_up_pic_no_1, 
+		:pick_up_address_2, :pick_up_pic_no_2, :pick_up_address_3, :pick_up_pic_no_3, :pick_up_address_4, :pick_up_pic_no_4,
+		:drop_point_address_1, :drop_point_pic_no_1, :drop_point_address_2, :drop_point_pic_no_2, :drop_point_address_3, 
+		:drop_point_pic_no_3, :drop_point_address_4, :drop_point_pic_no_4, :goods_description, :datetime, :special_request, 
+		:drop_point_date, :trucks_capacity, :truck_reg_no, :name, :ic, :phone_no, :delivery_date, :do_request_date, :do_collection_date, 
+		:do_number, :total_tonne, :payment_name, :tel_no, :account_no, :billing_name, :billing_amount, :billing_received, :billing_balance,
+		:invoice_date, :invoice, :delivered, :payment_calculate_gst, :payment_reverse_calculate_gst, :gst_total_value, :payment_with_gst,
+		:billing_with_gst, :billing_calculate_gst, :billing_reverse_calculate_gst, :gst_billing_total_value, :rate, :notes, :billing_rate, 
+		:driver_remarks
 		)
 end
-
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,38 +6,55 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.create_edit_order.subject
   #
-  def create_edit_order(order)
-    @order = order
-    if @order.user_email == "izzuddin@ikargo.com"
-      mail(to: "richard@ikargo.com",
-        bcc: "mu@ikargo.com",
-        cc: "aru@ikargo.com",
-        subject:  "New order has been created",
-        date: Time.now)
-    elsif @order.user_email == "mu@ikargo.com"
-      mail(to:"richard@ikargo.com",
-        bcc: "aru@ikargo.com",
-        cc: "izzuddin.muhamad@yahoo.com",
-        subject:  "New order has been created",
-        date: Time.now)
-    elsif @order.user_email == "richard@ikargo.com"
-      mail(to:"mu@ikargo.com",
-        bcc: "aru@ikargo.com",
-        cc: "izzuddin.muhamad@yahoo.com",
-        subject:  "New order has been created",
-        date: Time.now)
-    elsif @order.user_email == "aru@ikargo.com"
-      mail(to:"mu@ikargo.com",
-        bcc: "richard@ikargo.com",
-        cc: "izzuddin.muhamad@yahoo.com",
-        subject:  "New order has been created",
-        date: Time.now)
+  def create_edit_order(order, file)
+     @order = order
+     CSV.open(file, 'rb+', 
+      :write_headers => true,
+      :headers => ["Inquiry date", "No"]
+      ) do |csv|
+      csv << [@order.inquiry_date, @order.no]
     end
+    file.rewind
+    a = file.path
+    attachments['report.csv'] = File.read(a)
+    mail(to: "izzuddin@ikargo.com",
+      subject:  "New order has been created",
+      date: Time.now)
+
+    File.delete
+
+    # if @order.user_email == "izzuddin@ikargo.com"
+    #   mail(to: "richard@ikargo.com",
+    #     bcc: "mu@ikargo.com",
+    #     cc: "aru@ikargo.com",
+    #     subject:  "New order has been created",
+    #     date: Time.now)
+    # elsif @order.user_email == "mu@ikargo.com"
+    #   mail(to:"richard@ikargo.com",
+    #     bcc: "aru@ikargo.com",
+    #     cc: "izzuddin.muhamad@yahoo.com",
+    #     subject:  "New order has been created",
+    #     date: Time.now)
+    # elsif @order.user_email == "richard@ikargo.com"
+    #   mail(to:"mu@ikargo.com",
+    #     bcc: "aru@ikargo.com",
+    #     cc: "izzuddin.muhamad@yahoo.com",
+    #     subject:  "New order has been created",
+    #     date: Time.now)
+    # elsif @order.user_email == "aru@ikargo.com"
+    #   mail(to:"mu@ikargo.com",
+    #     bcc: "richard@ikargo.com",
+    #     cc: "izzuddin.muhamad@yahoo.com",
+    #     subject:  "New order has been created",
+    #     date: Time.now)
+    # end
+
+
   end
 
   def edit_order(order)
      @order = order
-     mail(to: "mu@ikargo.com",
+     mail(to: "izzuddin@ikargo.com",
      subject:  "Changes on shipment's details",
      date: Time.now)
   end
